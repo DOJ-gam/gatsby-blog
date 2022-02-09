c0f185a370dbd22d9ac910bf24eaf1bfe92ad6f7
DOJ <omarjeng999@gmail.com>
2022-02-09 02:48:50 +0000

    learn about pages and routes

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function about() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        saepe excepturi ex id, aperiam reprehenderit, numquam unde tempora optio
+        ipsam dolorem praesentium ab facilis dicta non sunt quis? Quae sit
+        accusamus voluptatibus, incidunt corrupti recusandae minus illo,
+        asperiores dicta nulla placeat velit delectus libero qui optio quasi
+        possimus? Aliquid ipsa pariatur voluptates magnam, nisi nemo, officia,
+        distinctio nihil minima ad eveniet vitae consequatur veritatis tenetur
+        cupiditate amet dolor voluptas quidem rem soluta maiores quae ipsam.
+        Iure beatae laborum delectus placeat!
+      </p>
+    </div>
+  )
+}
+
+export default about
